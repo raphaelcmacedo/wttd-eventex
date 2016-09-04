a2db6ae837afe65b00d52b74eb4c7f635a942566
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-09-04 15:45:18 -0300

    Email subscription

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 
 from eventex.subscriptions.forms import SubscriptionForm
@@ -30,4 +31,62 @@ class SubscribeTest(TestCase):
     def test_has_fields(self):
         form = self.response.context['form']
         self.assertSequenceEqual(['name','cpf','email','phone'], list(form.fields))
+
+
+class SubscriptionPostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Raphael Macedo', cpf='00000000000', email='raphaelcmacedo@hotmail.com',phone='(00) 00000-0000')
+        self.response = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        self.assertEqual(302, self.response.status_code)
+
+    def test_send_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        self.assertEqual(email.subject, 'Confirmação de inscrição')
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        self.assertEqual(email.from_email, 'contato@eventex.com.br')
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        self.assertEqual(email.from_email, 'contato@eventex.com.br')
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        self.assertIn('Raphael Macedo', email.body)
+        self.assertIn('00000000000', email.body)
+        self.assertIn('raphaelcmacedo@hotmail.com', email.body)
+        self.assertIn('(00) 00000-0000', email.body)
+
+
+class SubscriptionInvalidPost(TestCase):
+    def setUp(self):
+        self.response = self.client.post('/inscricao/',{})
+
+    def test_post(self):
+        self.assertEqual(200,self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, "subscriptions/subscription_form.html")
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form,SubscriptionForm)
+
+    def test_has_erros(self):
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
+
+class SubscribeSuccessMessage(TestCase):
+    def setUp(self):
+        data = dict(name='Raphael Macedo', cpf='00000000000', email='raphaelcmacedo@hotmail.com',phone='(00) 00000-0000')
+        self.response = self.client.post('/inscricao/', data, follow=True)
+
+    def test_message(self):
+        self.assertContains(self.response, 'Inscrição realizada com sucesso')
 
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,10 +1,33 @@
+from django.core import mail
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 
 
 # Create your views here.
+from django.template.loader import render_to_string
+
 from eventex.subscriptions.forms import SubscriptionForm
 
+def send_email(form):
+    subject = 'Confirmação de inscrição'
+    context = dict(name='Raphael Macedo', cpf='00000000000', email='raphaelcmacedo@hotmail.com',phone='(00) 00000-0000')
+    message = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+    from_email = 'contato@eventex.com.br'
+    to = ['contato@eventex.com.br', form.cleaned_data['email']]
+    mail.send_mail(subject, message, from_email, to)
 
 def subscribe(request):
-    context = {'form':SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            send_email(form)
+            messages.success(request, 'Inscrição realizada com sucesso')
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html', {"form":form})
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
+

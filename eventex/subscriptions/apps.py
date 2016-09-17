44c4b3ed11382184eae59066177d5b002f4a5c8e
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-09-17 11:25:44 -0300

    Admin config

---
+++ eventex/subscriptions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class SubscriprionsConfig(AppConfig):
+    name = 'eventex.subscriptions'
+    verbose_name = 'Participant Control'
--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Subscription (models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Name', max_length=100)
+    cpf = models.CharField('CPF', max_length=11)
+    email = models.EmailField('E-mail')
+    phone = models.CharField('Phone number', max_length=20)
+    created_at = models.DateTimeField('Created at',auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'subscription'
+        verbose_name_plural = 'subscriptions'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name

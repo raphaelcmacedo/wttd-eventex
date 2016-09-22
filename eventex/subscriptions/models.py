302f04d5d07c0378d7b3d7bee3af2a3cea49c3a0
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-09-21 22:21:07 -0300

    Add paid field

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -6,6 +6,7 @@ class Subscription (models.Model):
     email = models.EmailField('E-mail')
     phone = models.CharField('Phone number', max_length=20)
     created_at = models.DateTimeField('Created at',auto_now_add=True)
+    paid = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'subscription'

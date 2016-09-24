4bfadce5476eeee1ae298c8aa347bd3da57a616b
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-09-24 14:31:35 -0300

    Form subscription validation

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('CPF most contains only digits', 'digits')
+    if len(value) != 11:
+        raise ValidationError('CPF most have 11 characters', 'length')
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='E-mail')
     phone = forms.CharField(label='Phone')
+
+
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -9,3 +9,32 @@ class SubscriptionFormTest(TestCase):
     def test_has_fields(self):
         expected = ['name','cpf','email','phone']
         self.assertSequenceEqual(expected, list(self.form.fields))
+
+
+class SubscriptionValidation(TestCase):
+    def test_cpf_is_digit(self):
+        form = self.validated_form(cpf='A2345678901')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        form = self.validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def validated_form(self, **kwargs):
+        valid = dict(name='Raphael Macedo', email='a@a.com', cpf='12345678901', phone='0000-0000')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        return form
+
+    def assertFormErrorMessage(self, form, field, message):
+        errors = form.errors
+        errors_list = errors[field]
+        self.assertListEqual(errors_list, [message])
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(exception.code, code)
